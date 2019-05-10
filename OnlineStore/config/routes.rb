6b5e7a9c9b1c56e0dbff901bfe195e8071f6d1d7
ivailogeorgiev <ivailogeorgiev_2001@abv.bg>
2019-05-10 13:18:25 +0300

    email confirmation

--- OnlineStore/config/routes.rb
+++ OnlineStore/config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get "/users/make_admin", to: "home#make_admin"
   get "/home/login_page", to: "home#login_page"
   resources :cards
+
   devise_for :users
+
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
